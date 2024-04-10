5a9878aac1acb6bc86036e423d6dabfd846f8255
PavanKalyan <pavankalyan6md@gmail.com>
2024-04-10 20:10:39 +0530

    CafeItems Backend Code

--- src/main/java/com/brewedbrilliance/cafe/application/repository/UserRepository.java
+++ src/main/java/com/brewedbrilliance/cafe/application/repository/UserRepository.java
@@ -1,11 +1,8 @@
 package com.brewedbrilliance.cafe.application.repository;
 
-import com.brewedbrilliance.cafe.application.Entity.Coffee;
 import com.brewedbrilliance.cafe.application.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository <User, Integer> {
